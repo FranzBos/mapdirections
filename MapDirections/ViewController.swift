c452a3aa741f14dbad7d7921dce8f80c250450da
Francesco <franz.boschini@gmail.com>
2018-06-20 10:54:00 +0200

    first commit

--- MapDirections/ViewController.swift
+++ MapDirections/ViewController.swift
@@ -1,25 +1,67 @@
-//
-//  ViewController.swift
-//  MapDirections
-//
-//  Created by Francesco Boschini on 20/06/2018.
-//  Copyright © 2018 Francesco Boschini. All rights reserved.
-//
-
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        map.delegate = self
+        map.showsUserLocation = true
+        map.showsScale = true
+        map.showsPointsOfInterest = true
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        let sourceCoordinates = locationManager.location?.coordinate
+        let destinationCoordinates = CLLocationCoordinate2DMake(45.822497,8.7496685)
+        
+        let sourcePlacemark = MKPlacemark(coordinate: sourceCoordinates!)
+        let destinationPlacemark = MKPlacemark(coordinate: destinationCoordinates)
+        
+        let sourceItem = MKMapItem(placemark: sourcePlacemark)
+        let destinationItem = MKMapItem(placemark: destinationPlacemark)
+        
+        let directionRequest = MKDirectionsRequest()
+        directionRequest.source = sourceItem
+        directionRequest.destination = destinationItem
+        
+        directionRequest.transportType = .walking
+        
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate(completionHandler: {
+            response, error in
+            guard let response = response else {
+                if error != nil {
+                    print("Direction calculation error")
+                }
+                return
+            }
+            
+            let route = response.routes[0]
+            self.map.add(route.polyline, level: .aboveRoads)
+            
+            let rect = route.polyline.boundingMapRect
+            self.map.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
+        
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.blue
+        return renderer
     }
-
-
 }
 
